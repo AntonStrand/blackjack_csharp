7057e6d75e3f5c804e904ff8670f1ad625c5270f
antonstrand <jag@antonstrand.se>
2018-10-27 19:39:16 +0200

    Implement the IObserver interface

--- BlackJack/controller/PlayGame.cs
+++ BlackJack/controller/PlayGame.cs
@@ -3,39 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using BlackJack.view;
+using System.Threading;
 
 namespace BlackJack.controller
 {
-  class PlayGame
+  class PlayGame : model.IObserver
   {
-    public bool Play(model.Game a_game, view.IView a_view)
+    private const int SHOW_DELAY = 500;
+    private model.Game m_game;
+    private view.IView m_view;
+
+    public PlayGame(model.Game a_game, view.IView a_view)
     {
-      a_view.DisplayWelcomeMessage();
+      m_game = a_game;
+      m_view = a_view;
+      m_game.Attach(this);
+    }
 
-      a_view.DisplayDealerHand(a_game.GetDealerHand(), a_game.GetDealerScore());
-      a_view.DisplayPlayerHand(a_game.GetPlayerHand(), a_game.GetPlayerScore());
+    public bool Play()
+    {
+      m_view.DisplayWelcomeMessage();
 
-      if (a_game.IsGameOver())
+      m_view.DisplayDealerHand(m_game.GetDealerHand(), m_game.GetDealerScore());
+      m_view.DisplayPlayerHand(m_game.GetPlayerHand(), m_game.GetPlayerScore());
+
+      if (m_game.IsGameOver())
       {
-        a_view.DisplayGameOver(a_game.IsDealerWinner());
+        m_view.DisplayGameOver(m_game.IsDealerWinner());
       }
 
-      Input input = a_view.GetInput();
+      Input input = m_view.GetInput();
 
       if (input == Input.PlayGame)
       {
-        a_game.NewGame();
+        m_game.NewGame();
       }
       else if (input == Input.Hit)
       {
-        a_game.Hit();
+        m_game.Hit();
       }
       else if (input == Input.Stand)
       {
-        a_game.Stand();
+        m_game.Stand();
       }
 
       return input != Input.Quit;
     }
+    public void Update()
+    {
+      Thread.Sleep(SHOW_DELAY);
+      m_view.DisplayWelcomeMessage();
+
+      m_view.DisplayDealerHand(m_game.GetDealerHand(), m_game.GetDealerScore());
+      m_view.DisplayPlayerHand(m_game.GetPlayerHand(), m_game.GetPlayerScore());
+    }
   }
+
 }
